f39e835f949031e7744c8e0ac6fce253adf748e7
Luis Perichon <luisperichon+github@gmail.com>
2012-01-06 10:48:41 -0300

    back to normal migration

--- db/migrate/20111214014451_create_locations.rb
+++ db/migrate/20111214014451_create_locations.rb
@@ -1,7 +1,5 @@
 class CreateLocations < ActiveRecord::Migration
   def change
-    ActiveRecord::Base.establish_connection(::SpacialdbConnectionConfig) if Rails.env == "production"
-
     create_table :locations do |t|
       t.string :name
       t.point :latlon, :geographic => true
